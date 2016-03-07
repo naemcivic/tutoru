88ae00b559e04bfa67e629b3932b83eaf9f8aed1
naemcivic <naemcivic@gmail.com>
2016-03-07 14:00:56 -0500

    add multiDatespicker to appointments

--- app/assets/javascripts/users.js
+++ app/assets/javascripts/users.js
@@ -7,10 +7,18 @@ $(document).on('ready page :load', function() {
     $.getScript('/products?search=' + searchValue);
   });
 
+
 });
 
 $(document).on('page:change', function(){
+
+  $('#appointment_appointment_date').multiDatesPicker({
+    dateFormat: "yy-mm-dd"
+  });
+
   $('#user_profile_attributes_availability').multiDatesPicker({
     dateFormat: "yy-mm-dd"
   });
+
+
 });
--- app/controllers/appointments_controller.rb
+++ app/controllers/appointments_controller.rb
@@ -12,14 +12,14 @@ class AppointmentsController < ApplicationController
   end
 
   def create
-
     @appointment = @user.student_appointments.build(appointment_params)
     tutorid = @appointment.tutor_id
     tutor = User.find(tutorid)
+
     respond_to do |format|
       if @appointment.available?(@appointment.appointment_date, tutor.profile.availability)
         @appointment.save
-        format.html { redirect_to users_path, notice: 'Appointment made'}
+        format.html { redirect_to root_path, notice: 'Appointment made'}
         format.js {}
       else
         format.html { render root_path, alert: 'Try again'}
