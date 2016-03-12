26923b0238ee6834b233a78f6485ad0b775bb294
naemcivic <naemcivic@gmail.com>
2016-03-12 00:15:34 -0500

    add Cloudinary gem and implement cloud storage for file upload

--- app/uploaders/avatar_uploader.rb
+++ app/uploaders/avatar_uploader.rb
@@ -4,11 +4,26 @@ class AvatarUploader < CarrierWave::Uploader::Base
 
   # Include RMagick or MiniMagick support:
   # include CarrierWave::RMagick
+  include Cloudinary::CarrierWave
   include CarrierWave::MiniMagick
 
   # Choose what kind of storage to use for this uploader:
-  storage :file
+  # storage :file
   # storage :fog
+
+  version :standard do
+    process :eager => true
+    process :resize_to_fill => [100, 150, :north]
+  end
+
+  version :thumbnail do
+    eager
+    resize_to_fit(80, 80)
+  end
+
+
+
+
 
   # Override the directory where uploaded files will be stored.
   # This is a sensible default for uploaders that are meant to be mounted:
