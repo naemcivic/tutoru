98e6f0ab19aff2faa97f6670ad6c31d54eca1cd8
naemcivic <naemcivic@gmail.com>
2016-03-12 21:29:01 -0500

    add pin drop on map

--- app/assets/javascripts/maps.js
+++ app/assets/javascripts/maps.js
@@ -3,12 +3,13 @@ function initMap(){
   var latLong = {lat: mapPlaceHolder.data('latitude'), lng: mapPlaceHolder.data('longitude')};
   var map = new google.maps.Map(mapPlaceHolder[0],{
     center: latLong,
-    zoom: 14
+    zoom: 14,
+  fullscreenControl: true,
   });
 
   var marker = new google.maps.Marker({
     position: latLong,
     map: map,
-    title: 'Hello World!'
+    animation: google.maps.Animation.DROP
   });
 }
